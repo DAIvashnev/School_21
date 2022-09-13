d89901e6674a2f92cc7ec607fbbb64f37d2341ae
enedinae <ida94b@mail.ru>
2022-09-13 21:05:44 +0300

    edit_switch

--- my_cat.c
+++ my_cat.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     FILE *fp;
     int f = 0;
     int c;
-    if((fp = fopen(*++argv, "r")) == NULL) {
+    char *check = argv[1];
+    if(check[0] == '-') {
+        f = 1;
+        switch (check[1]) {
+            case 'n' :
+                printf("HOOOLLLAAA\n"); break;
+            case 'v' :
+                printf("777777777777\n"); break;
+            default : 
+                break;
+        }
+    }
+    if((fp = fopen(*++argv, "r")) == NULL && f == 0) {
         printf("error: can't open %s\n", *argv);
         f = 1;
     } else if(f == 0) {
+        int p = 0;
         while((c = getc(fp)) != EOF) {
+            p++;
             putc(c, stdout);
+        }
+        if(p == 0) {
+            printf("File %s is empty.\n", *argv);
         }
         fclose(fp);
     }
