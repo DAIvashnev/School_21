4837d200d4528fd83e180e6834a58ca65ee22661
enedinae <ida94b@mail.ru>
2022-09-16 23:02:58 +0300

    b flag

--- my_cat.c
+++ my_cat.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-void my_cat(char *argv);
+void my_cat(char *argv, char key, int *num);
 
-void my_cat(char *argv) {
+void my_cat(char *argv, char key, int *num) {
     FILE *fp;
     int f = 0;
     int c;
@@ -11,16 +11,33 @@ void my_cat(char *argv) {
         printf("my_cat: %s: There is no such file or directory\n", argv);
         f = 1;
     } else if(f == 0) {
-        while((c = getc(fp)) != EOF) {
+        if(key == 'b') {
+            int check = 0;
+            while((c = getc(fp)) != EOF) {
+                if(c != '\n' && check == 0) {
+                    check = 1;
+                    *num += 1;
+                    printf("%d  ", *num);
+                }
+                putc(c, stdout);
+                if(c == '\n') {
+                    check = 0;
+                }
+            }
+            fclose(fp);
+        }
+        /*while((c = getc(fp)) != EOF) {
             putc(c, stdout);
         }
-        fclose(fp);
+        fclose(fp);*/
     }
 }
 
 int main(int argc, char *argv[]) {
     FILE *ch;
     char pt_c;
+    char key;
+    int num = 0;
     if(argc == 1) {
         while(1) {
             scanf("%c", &pt_c);
@@ -30,14 +47,26 @@ int main(int argc, char *argv[]) {
     char *check = argv[1];
     if(check[0] == '-') {
         switch (check[1]) {
+            case 'b' : 
+                if(argc >= 2) {
+                    while(*argv) {
+                        argv++;
+                        if((ch = fopen(*argv, "r")) == NULL) {
+                            if(*argv == NULL) {
+                                break;
+                            }
+                            if(!(argv[1])) {
+                                printf("my_cat: %s: There is no such file or directory\n", *argv);
+                            }
+                        } else {
+                            key = 'b';
+                            my_cat(*argv, key, &num);
+                        }
+                    }
+                } break;
             case 'n' :
                 switch (check[2]) {
                     case '\0' : printf("HOOOLLLAAA\n"); break;
-                    default : printf("my_cat: invalid key - «%s»\n", check); break;
-                } break;
-            case 'v' : 
-                switch (check[2]) {
-                    case '\0' : printf("777777777777\n"); break;
                     default : printf("my_cat: invalid key - «%s»\n", check); break;
                 } break;
             default : printf("my_cat: invalid key - «%s»\n", check); break;
@@ -51,7 +80,7 @@ int main(int argc, char *argv[]) {
                 }
                 printf("my_cat: %s: There is no such file or directory\n", *argv);
             } else {
-                my_cat(*argv);
+                my_cat(*argv, key, &num);
             }
         }
     }
