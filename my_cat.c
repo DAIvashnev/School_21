17acf5c3824e8e135222fd4ff099fae15437e44e
enedinae <ida94b@mail.ru>
2022-09-16 19:17:03 +0300

    cat without flags

--- my_cat.c
+++ my_cat.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+void my_cat(char *argv);
+
+void my_cat(char *argv) {
     FILE *fp;
     int f = 0;
     int c;
+    if((fp = fopen(argv, "r")) == NULL && f == 0) {
+        printf("my_cat: %s: There is no such file or directory\n", argv);
+        f = 1;
+    } else if(f == 0) {
+        while((c = getc(fp)) != EOF) {
+            putc(c, stdout);
+        }
+        fclose(fp);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    FILE *ch;
+    char pt_c;
+    if(argc == 1) {
+        while(1) {
+            scanf("%c", &pt_c);
+            printf("%c", pt_c);
+        }
+    }
     char *check = argv[1];
     if(check[0] == '-') {
-        f = 1;
         switch (check[1]) {
             case 'n' :
-                printf("HOOOLLLAAA\n"); break;
-            case 'v' :
-                printf("777777777777\n"); break;
-            default :
-                printf("my_cat: %s: There is no such file or directory\n", *argv); break;
+                switch (check[2]) {
+                    case '\0' : printf("HOOOLLLAAA\n"); break;
+                    default : printf("my_cat: invalid key - «%s»\n", check); break;
+                } break;
+            case 'v' : 
+                switch (check[2]) {
+                    case '\0' : printf("777777777777\n"); break;
+                    default : printf("my_cat: invalid key - «%s»\n", check); break;
+                } break;
+            default : printf("my_cat: invalid key - «%s»\n", check); break;
+        }
+    } else {
+        while(*argv) {
+            argv++;
+            if((ch = fopen(*argv, "r")) == NULL) {
+                if(*argv == NULL) {
+                    break;
+                }
+                printf("my_cat: %s: There is no such file or directory\n", *argv);
+            } else {
+                my_cat(*argv);
+            }
         }
     }
-    while(*argv) {
-        if((fp = fopen(*++argv, "r")) == NULL && f == 0) {
-            printf("my_cat: %s: There is no such file or directory\n", *argv);
-            f = 1;
-        } else if(f == 0) {
-            while((c = getc(fp)) != EOF) {
-                putc(c, stdout);
-            }
-            fclose(fp);
-        }
-        if((fp = fopen(*++argv, "r")) == NULL) {
-            if(*argv == NULL) {
-                break;
-            }
-            printf("my_cat: %s: There is no such file or directory\n", *argv);
-        } else {
-            *--argv;
-            fclose(fp);
-        }
-    }
-    return f;
+    return 0;
 }
