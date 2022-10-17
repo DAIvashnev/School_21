27a09ad7981a32a85001d62daadf27ffc7e872a5
enedinae <ida94b@mail.ru>
2022-10-17 22:03:03 +0300

    start flags

--- Cat_Grep/my_grep.c
+++ Cat_Grep/my_grep.c
@@ -3,10 +3,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+void check_key(char *argv, char **key);
 int cheking_file(char *argv);
-void string_selection(const char *search_str, char *argv, int *argc);
-void check_search(const char *search_str, char *choice_str, int **argc, char *argv);
-void output(char *choice_str, int ***argc, char *argv);
+void string_selection(char *search_str, char *argv, int *argc);
+int check_search(char *search_str, char *choice_str, char *argv);
+void output(char *choice_str, char *argv, int **argc);
+
+void check_key(char *argv, char **key) {
+    if(argv[0] == '-') {
+        *key = argv;
+    }
+}
 
 int cheking_file(char *argv) {
     FILE *fp;
@@ -20,8 +27,7 @@ int cheking_file(char *argv) {
     return flag;
 }
 
-
-void string_selection(const char *search_str, char *argv, int *argc) {
+void string_selection(char *search_str, char *argv, int *argc) {
     FILE *fp;
     int c;
     char *choice_str = NULL;
@@ -33,19 +39,24 @@ void string_selection(const char *search_str, char *argv, int *argc) {
         i++;
         if(c == '\n') {
             choice_str[i] = '\0';
-            check_search(search_str, choice_str, &argc, argv);
+            if(check_search(search_str, choice_str, argv) == 1) {
+                output(choice_str, argv, &argc);
+            }
             i = 0;
         }
     }
     choice_str[i] = '\n';
     choice_str[i+1] = '\0';
-    check_search(search_str, choice_str, &argc, argv);
+    if(check_search(search_str, choice_str, argv) == 1) {
+        output(choice_str, argv, &argc);
+    }
     free(choice_str);
     fclose(fp);
 }
 
-void check_search(const char *search_str, char *choice_str, int **argc, char *argv) {
-    int j = 0;
+int check_search(char *search_str, char *choice_str, char *argv) {
+    size_t j = 0;
+    int flag = 0;
     char *check_str = NULL;
     for(int i = 0; choice_str[i] != '\0'; i++) {
         if(choice_str[i] == search_str[j]) {
@@ -53,17 +64,20 @@ void check_search(const char *search_str, char *choice_str, int **argc, char *ar
             check_str[j] = search_str[j];
             j++;
             if(strlen(search_str) == j && strcmp(search_str, check_str) == 0) {
-                output(choice_str, &argc, argv);
+                flag = 1;
             }
         } else {
             j = 0;
         }
     }
     free(check_str);
+
+    return flag;
 }
 
-void output(char *choice_str, int ***argc, char *argv) {
-    if(***argc > 3) {
+void output(char *choice_str, char *argv, int **argc) {
+    int check_flag = 0;
+    if(**argc > 3) {
         printf("%s:%s", argv, choice_str);
     } else {
         printf("%s", choice_str);
@@ -71,7 +85,17 @@ void output(char *choice_str, int ***argc, char *argv) {
 }
 
 int main(int argc, char *argv[]) {
-    int arguments = 2;
+    int arguments = 0;
+    char *key = "0";
+    while(argv[arguments]) {
+        check_key(argv[arguments], &key);
+        arguments++;
+    }
+    if(key[0] == '-') { 
+        arguments = 3; 
+    } else { 
+        arguments = 2; 
+    }
     while(argv[arguments]) {
         if(cheking_file(argv[arguments]) == 0) {
             string_selection(argv[1], argv[arguments], &argc);
